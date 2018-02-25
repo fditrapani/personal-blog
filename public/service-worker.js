5556a4d9a63d3da2fe2e57a05b7bca166211d372
fditrapani <filippo.di.trapani@automattic.com>
2018-02-25 11:22:14 -0500

    Fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -38,7 +38,7 @@ self.addEventListener('install', function(event) {
               // We could also cache any static assets like CSS or images
               const urlsToCache = [
                 "/",
-                assets["App.js"]
+                assets["main.js"]
               ]
               cache.addAll(urlsToCache)
               console.log('cached');
