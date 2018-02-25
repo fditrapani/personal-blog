4ceac17378158406d3587930122d11676768e22b
fditrapani <filippo.di.trapani@automattic.com>
2018-02-24 23:41:21 -0500

    Polishc ontainers

--- src/components/appshell/index.js
+++ src/components/appshell/index.js
@@ -3,6 +3,10 @@ import Navigation from '../navigation/';
 import '../../sass/App.css';
 
 class AppShell extends Component {
+  componentWillReceiveProps() {
+     this.refs.appShellContainer.scrollTop = 0;
+  }
+  
   render() {
     const { children } = this.props;
 
@@ -10,7 +14,7 @@ class AppShell extends Component {
       <div className="app-shell">
         <Navigation />
 
-        <div className="app-shell__content">
+        <div ref="appShellContainer" className="app-shell__content">
            { children }
         </div>
       </div>
