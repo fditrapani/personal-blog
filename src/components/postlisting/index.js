79e4e20c05645907418e9ca5fab4dd22e35ae134
fditrapani <filippodt@gmail.com>
2022-04-17 09:10:01 -0400

    Fix svg errors

--- src/components/postlisting/index.js
+++ src/components/postlisting/index.js
@@ -42,8 +42,8 @@ export default class PostListing extends React.Component {
         return (
           <span className="post-listing__visited post-listing__icon">
             <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M11 6.5L6.875 10.5L5 8.68182" stroke="#EAEAEA" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-              <rect x="0.666667" y="1.16667" width="14.6667" height="14.6667" rx="7.33333" stroke="#EAEAEA" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M11 6.5L6.875 10.5L5 8.68182" stroke="#EAEAEA" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
+              <rect x="0.666667" y="1.16667" width="14.6667" height="14.6667" rx="7.33333" stroke="#EAEAEA" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
 
 
@@ -58,10 +58,10 @@ export default class PostListing extends React.Component {
     return(
       <span className="post-listing__date post-listing__icon">
         <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12.6667 3.16667H3.33333C2.59695 3.16667 2 3.76363 2 4.50001V13.8333C2 14.5697 2.59695 15.1667 3.33333 15.1667H12.6667C13.403 15.1667 14 14.5697 14 13.8333V4.50001C14 3.76363 13.403 3.16667 12.6667 3.16667Z" stroke="#EAEAEA" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M10.6667 1.83333V4.49999" stroke="#EAEAEA" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M5.33337 1.83333V4.49999" stroke="#EAEAEA" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M2 7.16667H14" stroke="#EAEAEA" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M12.6667 3.16667H3.33333C2.59695 3.16667 2 3.76363 2 4.50001V13.8333C2 14.5697 2.59695 15.1667 3.33333 15.1667H12.6667C13.403 15.1667 14 14.5697 14 13.8333V4.50001C14 3.76363 13.403 3.16667 12.6667 3.16667Z" stroke="#EAEAEA" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M10.6667 1.83333V4.49999" stroke="#EAEAEA" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M5.33337 1.83333V4.49999" stroke="#EAEAEA" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M2 7.16667H14" stroke="#EAEAEA" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
 
         { date }
