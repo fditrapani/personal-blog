1a53f10af5a8a2757a6fc480c07c6296250169d9
fditrapani <filippodt@gmail.com>
2024-02-26 20:57:35 -0500

    Fix button sizes

--- src/components/icons/navigationIcon/index.js
+++ src/components/icons/navigationIcon/index.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class NavigationIcon extends Component {
 	render() { 
 		return (
-      <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path opacity="0.05" fillRule="evenodd" clipRule="evenodd" d="M14 26C20.6274 26 26 20.6274 26 14C26 7.37258 20.6274 2 14 2C7.37258 2 2 7.37258 2 14C2 20.6274 7.37258 26 14 26ZM14.6637 9.34315L10.0002 14.9711L11.6555 16.3427L11.6556 16.3427L15.6284 19.6346L17 17.9793L13.0272 14.6874L16.319 10.7147L14.6637 9.34315Z" fill="black"/>
-        <path fillRule="evenodd" clipRule="evenodd" d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24ZM12.6637 7.34315L8.00023 12.9711L9.65553 14.3427L9.6556 14.3427L13.6284 17.6346L15 15.9793L11.0272 12.6874L14.319 8.71475L12.6637 7.34315Z" fill="white"/>
+      <svg width="26" height="26" viewBox="0 0 26 26" className="navigation-icon" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path opacity="0.05" fillRule="evenodd" clipRule="evenodd" d="M14 24C20.6274 24 26 18.6274 26 12C26 5.37258 20.6274 0 14 0C7.37258 0 2 5.37258 2 12C2 18.6274 7.37258 24 14 24ZM14.6637 7.34315L10.0002 12.9711L11.6555 14.3427L11.6556 14.3427L15.6284 17.6346L17 15.9793L13.0272 12.6874L16.319 8.71475L14.6637 7.34315Z" fill="black"/>
+        <path opacity="0.9" fillRule="evenodd" clipRule="evenodd" d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24ZM12.6637 7.34315L8.00023 12.9711L9.65553 14.3427L9.6556 14.3427L13.6284 17.6346L15 15.9793L11.0272 12.6874L14.319 8.71475L12.6637 7.34315Z" fill="white"/>
       </svg>
 		);
 	}
