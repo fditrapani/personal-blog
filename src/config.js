8490a008314a8564c9cd27375fa497904117387f
Filippo Di Trapani <filippodt@gmail.com>
2025-04-06 22:08:41 -0400

    Update url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const config = {
 	url: "https://filippodt.com",
 	non_https_url: "http://filippodt.com",
-	wordpress_url: "fditrapani.wordpress.com",
+	wordpress_url: "fditrapaniportfolio.wordpress.com",
 	rss_feed: "https://fditrapani.wpcomstaging.com/feed/",
 	siteTitle: "The personal blog of Filippo Di Trapani",
 	siteBanner: "https://filippodt.com/images/filippodt-com.jpg",
