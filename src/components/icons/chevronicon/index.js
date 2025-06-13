9101df6403c37b4f73a25f77f4f7ce6fb060c33d
Filippo Di Trapani <filippodt@gmail.com>
2025-06-13 14:58:56 -0400

    Tweak animations

--- src/components/icons/chevronicon/index.js
+++ src/components/icons/chevronicon/index.js
@@ -6,8 +6,7 @@ class Chevronicon extends Component {
 			<svg className="icon icon--chevron" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<path color="currentColor" d="M5 12H19" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 				<path d="M12 5L19 12L12 19" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-			</svg>
-			
+			</svg>			
 		);
 	}
 
--- src/components/icons/navigationIcon/index.js
+++ src/components/icons/navigationIcon/index.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 class NavigationIcon extends Component {
 	render() { 
 		return (
-      <svg width="26" height="26" viewBox="0 0 26 26" className="navigation-icon" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path opacity="0.05" fillRule="evenodd" clipRule="evenodd" d="M14 24C20.6274 24 26 18.6274 26 12C26 5.37258 20.6274 0 14 0C7.37258 0 2 5.37258 2 12C2 18.6274 7.37258 24 14 24ZM14.6637 7.34315L10.0002 12.9711L11.6555 14.3427L11.6556 14.3427L15.6284 17.6346L17 15.9793L13.0272 12.6874L16.319 8.71475L14.6637 7.34315Z" fill="black"/>
-        <path opacity="0.9" fillRule="evenodd" clipRule="evenodd" d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24ZM12.6637 7.34315L8.00023 12.9711L9.65553 14.3427L9.6556 14.3427L13.6284 17.6346L15 15.9793L11.0272 12.6874L14.319 8.71475L12.6637 7.34315Z" fill="white"/>
+      <svg className="navigation-icon" width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path opacity="0.05" fill-rule="evenodd" clip-rule="evenodd" d="M14 24C20.6274 24 26 18.6274 26 12C26 5.37258 20.6274 0 14 0C7.37258 0 2 5.37258 2 12C2 18.6274 7.37258 24 14 24ZM14.6637 7.34315L10.0002 12.9711L11.6555 14.3427L15.6284 17.6346L17 15.9793L13.0272 12.6874L16.319 8.71475L14.6637 7.34315Z" fill="black"/>
+        <path opacity="0.9" fill-rule="evenodd" clip-rule="evenodd" d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24ZM12.6637 7.34315L8.00023 12.9711L9.65553 14.3427L13.6284 17.6346L15 15.9793L11.0272 12.6874L14.319 8.71475L12.6637 7.34315Z" fill="white"/>
+        <path d="M14.3193 8.71484L11.0273 12.6875L15 15.9795L13.6279 17.6348L9.65527 14.3428L8 12.9707L12.6641 7.34277L14.3193 8.71484Z" fill="#262326"/>
       </svg>
+      
 		);
 	}
 
